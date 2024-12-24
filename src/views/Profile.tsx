46eb909d9d18e1c53487f2dab8420d377d413073
Giovanni Almeida <giovanni.as.br@gmail.com>
2024-12-23 23:42:47 -0300

    feat: implement ImagePicker on Profile view & Toast error messages

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,25 +1,74 @@
+import { useState } from "react";
 import { ScrollView, TouchableOpacity } from "react-native";
-import { VStack, Center, Text, Heading } from "@gluestack-ui/themed";
+
+import { VStack, Center, Text, Heading, useToast } from "@gluestack-ui/themed";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 import { DefaultHeader } from "@components/DefaultHeader";
 import { UserImage } from "@components/UserImage";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
+import UserPhotoDefault from "@assets/userPhotoDefault.png";
+import { ToastMessage } from "@components/ToastMessage";
+
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState<string>(UserPhotoDefault);
+
+  const toast = useToast();
+
+  async function handleChangePhoto() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: "images",
+        allowsEditing: true,
+        aspect: [4, 4],
+        quality: 1,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      const photoURI = photoSelected.assets[0].uri;
+
+      if (photoURI) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as {
+          size: number;
+        };
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            placement: "top",
+            render: ({ id }) => (
+              <ToastMessage
+                id={id}
+                action="error"
+                title="Photo too large"
+                description="Please choose a photo smaller than 5MB"
+                onClose={() => toast.close(id)}
+              />
+            ),
+          });
+        }
+
+        setUserPhoto(photoURI);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <DefaultHeader title="Profile" />
 
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
-          <UserImage
-            source={require("@assets/user.png")}
-            alt="User Image"
-            size="xl"
-          />
+          <UserImage source={userPhoto} alt="User Image" size="xl" />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleChangePhoto}>
             <Text
               color="$primary300"
               fontFamily="$heading"
